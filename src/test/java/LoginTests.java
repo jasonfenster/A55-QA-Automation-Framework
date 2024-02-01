d4f4da792d221a0e619167c4685d38afe78c44fb
jasonfenster <jason.fenster82@gmail.com>
2024-02-01 11:14:52 -0500

    Sign and and song added to playlist

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
@@ -17,8 +19,20 @@ public class LoginTests extends BaseTest {
         WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        String url = "https://qa.koel.app/";
+        String url = "https://ga.koel.app/";
         driver.get(url);
+
+        WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
+       emailField.clear();
+       emailField.sendKeys("jason.fenstermaker1234@testpro.io");
+
+         WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
+       passwordField.clear();
+       passwordField.sendKeys("Testpro.io2");
+
+       WebElement loginBtn = driver.findElement(By.cssSelector("button[type='submit']"));
+       loginBtn.click();
+
         Assert.assertEquals(driver.getCurrentUrl(), url);
         driver.quit();
     }
