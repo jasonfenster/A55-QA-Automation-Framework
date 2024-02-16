d13a4c2200089a70638966ac69769b237b2efdfc
jasonfenster <jason.fenster82@gmail.com>
2024-02-16 11:30:41 -0500

    Add Selenium Grid

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,7 +4,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -13,6 +19,9 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Parameters;
 
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
 import java.time.Duration;
 
 import static org.bouncycastle.cms.RecipientId.password;
@@ -35,16 +44,21 @@ public class BaseTest {
 
     @BeforeSuite
     static void setupClass() {
-        WebDriverManager.chromedriver().setup();
+        //WebDriverManager.chromedriver().setup();
+        //WebDriverManager.firefoxdriver().setup();
+        //WebDriverManager.safaridriver().setup();
     }
 
     @BeforeMethod
     @Parameters({"BaseURL"})
-    public void launchBrowser(String baseURL){
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
+    public void launchBrowser(String baseURL) throws MalformedURLException {
+        //ChromeOptions options = new ChromeOptions();
+        //options.addArguments("--remote-allow-origins=*");
 
-        driver = new ChromeDriver(options);
+        //driver = new ChromeDriver(options);
+        //driver = new FirefoxDriver();
+        //driver = new SafariDriver();
+        driver = pickBrowser(System.getProperty("browser"));
         //Implicate Wait
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         //Explicate Wait
@@ -53,6 +67,33 @@ public class BaseTest {
         actions = new Actions(driver);
 
         navigateToPage(baseURL);
+    }
+
+    public WebDriver pickBrowser(String browser) throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        String gridURL = "http://192.168.40.46:4444/";
+        switch (browser){
+            //case "firefox":
+                //WebDriverManager.firefoxdriver().setup();
+                //return driver = new FirefoxDriver();
+            //case "Microsoft Edge":
+                //WebDriverManager.edgedriver().setup();
+                //EdgeOptions edgeOptions = new EdgeOptions();
+                //edgeOptions.addArguments("--remote-allow-origins=*");
+                //return driver = new EdgeDriver(edgeOptions);
+            case "grid-safari":
+                caps.setCapability("browserName", "safari");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(),caps);
+            case "grid-firefox":
+                caps.setCapability("browserName", "firefox");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(),caps);
+
+            default:
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--remote-allow-origins=*");
+                return driver = new ChromeDriver(chromeOptions);
+        }
     }
 
     @AfterMethod
