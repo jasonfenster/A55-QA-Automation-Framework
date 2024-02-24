03d7bcae5e97af3169ba9c520b64dce8d8aac096
jasonfenster <jason.fenster82@gmail.com>
2024-02-24 15:11:12 -0500

    Stil not right but need to send in for answers

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -29,7 +29,7 @@ import java.util.HashMap;
 import static org.bouncycastle.cms.RecipientId.password;
 
 public class BaseTest {
-
+   public ThreadLocal<WebDriver> threadDriver = new ThreadLocal<>();
 
     public WebDriver driver;
 
@@ -46,7 +46,7 @@ public class BaseTest {
 
     @BeforeSuite
     static void setupClass() {
-        //WebDriverManager.chromedriver().setup();
+        WebDriverManager.chromedriver().setup();
         //WebDriverManager.firefoxdriver().setup();
         //WebDriverManager.safaridriver().setup();
     }
@@ -54,6 +54,11 @@ public class BaseTest {
     @BeforeMethod
     @Parameters({"BaseURL"})
     public void launchBrowser(String baseURL) throws MalformedURLException {
+        threadDriver.set(pickBrowser(System.getProperty("browser")));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        actions = new Actions(driver);
         //ChromeOptions options = new ChromeOptions();
         //options.addArguments("--remote-allow-origins=*");
 
@@ -74,27 +79,18 @@ public class BaseTest {
     public WebDriver pickBrowser(String browser) throws MalformedURLException {
         DesiredCapabilities caps = new DesiredCapabilities();
         String gridURL = "http://192.168.40.46:4444/";
-        switch (browser){
-            //case "firefox":
-                //WebDriverManager.firefoxdriver().setup();
-                //return driver = new FirefoxDriver();
-            //case "Microsoft Edge":
-                //WebDriverManager.edgedriver().setup();
-                //EdgeOptions edgeOptions = new EdgeOptions();
-                //edgeOptions.addArguments("--remote-allow-origins=*");
-                //return driver = new EdgeDriver(edgeOptions);
+
+        switch (browser) {
             case "grid-safari":
                 caps.setCapability("browserName", "safari");
-                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(),caps);
+                return new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
             case "grid-firefox":
                 caps.setCapability("browserName", "firefox");
-                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(),caps);
-
+                return new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
             case "cloud":
                 return lambdaTest();
-
             default:
-                WebDriverManager.chromedriver().setup();
+                WebDriverManager.chromedriver().setup(); // Move this line here
                 ChromeOptions chromeOptions = new ChromeOptions();
                 chromeOptions.addArguments("--remote-allow-origins=*");
                 return driver = new ChromeDriver(chromeOptions);
@@ -135,9 +131,14 @@ public class BaseTest {
     }
 
     @AfterMethod
-    public void closeBrowser(){
-        driver.quit();
+    public void tearDown() {
+        threadDriver.get().close();
+        threadDriver.remove();
     }
+
+    //public void closeBrowser(){
+      //  driver.quit();
+    //}
 
     public void loginToKoel(){
         //WebElement loginBtn = driver.findElement(By.cssSelector("button[type='submit']"));
